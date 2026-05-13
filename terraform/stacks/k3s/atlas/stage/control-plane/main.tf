e19cfb908fb577f670e9c7d338d449c1466f7e3c
Pierce LeVangie <myriadgh@levangie.org>
2026-05-13 11:42:12 -0400

    Update Atlas VM storage defaults to flash

--- terraform/stacks/k3s/atlas/stage/control-plane/main.tf
+++ terraform/stacks/k3s/atlas/stage/control-plane/main.tf
@@ -108,7 +108,7 @@ variable "nic_name" {
 variable "vm_storage" {
   description = "Storage pool for control-plane disks."
   type        = string
-  default     = "tank"
+  default     = "flash"
 }
 
 variable "cpu_cores" {
